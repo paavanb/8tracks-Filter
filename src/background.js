b7b0faaa13e9cef3e0052f89fbcc2d399c8d4c7b
Paavan Bhavsar <pbhavsar@kyruus.com>
2016-04-18 13:13:26 -0400

    Fixed indentation issues

--- src/background.js
+++ src/background.js
@@ -1,57 +1,62 @@
-var icon_filter_off = {"19": "img/filter_off_19.png",
-					  "38": "img/filter_off_38.png"}
+var icon_filter_off = {
+    "19": "img/filter_off_19.png",
+    "38": "img/filter_off_38.png"
+}
 
-var icon_filter_on = {"19": "img/filter_on_19.png",
-					 "38": "img/filter_on_38.png"}
+var icon_filter_on = {
+    "19": "img/filter_on_19.png",
+    "38": "img/filter_on_38.png"
+}
+
 // make it easy to set the icon
 icon = {"on": icon_filter_on, "off": icon_filter_off}
 
 function initialize() {
-	chrome.storage.local.set(({state: "on"}))
+    chrome.storage.local.set(({state: "on"}))
 }
 
 function setIcon(tabId) {
-	chrome.storage.local.get("state", function (result) {
-		chrome.pageAction.setTitle({tabId: tabId, title: "8tracks filter " + result["state"]})
+    chrome.storage.local.get("state", function (result) {
+        chrome.pageAction.setTitle({tabId: tabId, title: "8tracks filter " + result["state"]})
 
-		chrome.pageAction.setIcon({tabId: tabId, path: icon[result["state"]]})
-	})
+        chrome.pageAction.setIcon({tabId: tabId, path: icon[result["state"]]})
+    })
 }
 
 function checkValidUrl(tab) {
-	if(tab.url.indexOf("8tracks.com") > 0) {
-    	chrome.pageAction.show(tab.id)
-    	// need to reset the icon to reflect the state
-    	setIcon(tab.id);
-  }
+    if(tab.url.indexOf("8tracks.com") > 0) {
+        chrome.pageAction.show(tab.id)
+        // need to reset the icon to reflect the state
+        setIcon(tab.id);
+    }
 }
 
 
 //for the onUpdated listener
 function checkValidUrlUpdated(tabId, changeInfo, tab) {
-	checkValidUrl(tab)
+    checkValidUrl(tab)
 }
 
 //for the onReplaced listener (in case page is loaded from cache)
 function checkValidUrlReplaced(addedTabId, removedTabId) {
-	chrome.tabs.get(addedTabId, checkValidUrl)
+    chrome.tabs.get(addedTabId, checkValidUrl)
 }
 
 function toggle(tab) {
-  	chrome.storage.local.get("state", function (result) {
-  					newState = ""
-					if(result["state"] == "on") {
-					  newState = "off"
-					} else {
-					  newState = "on"
-					}
-					//update the little tooltip to reflect current state
-					chrome.pageAction.setTitle({tabId: tab.id, title: "8tracks Filter " + newState})
-					//so that we update the page with the new filter settings
-					chrome.tabs.reload()
-					//store the state of the new filter settings so we remember across browser sessions
-					chrome.storage.local.set({state: newState})
-})
+    chrome.storage.local.get("state", function (result) {
+        newState = ""
+        if(result["state"] == "on") {
+            newState = "off"
+        } else {
+            newState = "on"
+        }
+        //update the little tooltip to reflect current state
+        chrome.pageAction.setTitle({tabId: tab.id, title: "8tracks Filter " + newState})
+        //so that we update the page with the new filter settings
+        chrome.tabs.reload()
+        //store the state of the new filter settings so we remember across browser sessions
+        chrome.storage.local.set({state: newState})
+    })
 }
 
 chrome.runtime.onInstalled.addListener(initialize);
--- src/filter.js
+++ src/filter.js
@@ -1,9 +1,9 @@
 function filter() {
-  chrome.storage.local.get("state", function(result) { 
-  										if(result["state"] == "on") { 
-  											$("img.cover").remove() 
-  										} 
-  									});
+  chrome.storage.local.get("state", function(result) {
+      if(result["state"] == "on") {
+          $("img.cover").remove()
+      }
+  });
 }
 // Make sure that when we navigate the site (which doesn't refresh the page), the filter is still run
 $('#body_container').bind('DOMSubtreeModified',filter);
